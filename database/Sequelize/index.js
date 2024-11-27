a1fe375c6cd51ce93caf16a97828c17638c9d87b
William Brendel <brendel.william@gmail.com>
2024-11-27 14:48:14 -0800

    sequelize

--- database/Sequelize/index.js
+++ database/Sequelize/index.js
@@ -21,23 +21,23 @@ const _msg = (where, text) => formatLogMsg(where, text, '📀 SQL:', 'white'),
 const DEFAULT_CONFIG = Object.freeze({
 	connectionString: '',
 	models: {},
-	pool: {
+	pool: Object.freeze({
 		max: 10, // maximum number of 'opened' connections to be scaled to
 		min: 1, // minimum number of 'opened' connections, can be 0
 		acquire: 1000, // connect timeout in ms
 		idle: 10000, // how long we keep open connections before scaling down, in ms
-	},
+	}),
 	logging: false, // turn logging on | off, can be a logging function or true or false or null or undefined
-	dialectOptions: {
+	dialectOptions: Object.freeze({
 		connectTimeout: 1000, // connection timeout in ms, similar to the pool.aquire option
 		multipleStatements: true // for running raw queries with semicolons, to support mutiple statement per query
-	},
-	define: {
+	}),
+	define: Object.freeze({
 		freezeTableName: true, // turn off pluralization on model names and getters
 		underscored: true, // model name from camel case to underscore notation
 		charset: 'utf8mb4', // character encoding
 		collate: 'utf8mb4_general_ci' // character encoding
-	},
+	}),
   heartbeatPingFrequency: 120000 // in ms - ping/pong sent to keep the MariaDB connection alive.
 });
 
