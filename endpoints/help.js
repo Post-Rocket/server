97e91445a4feaab011285a2f780a645bb97b77b7
William Brendel <brendel.william@gmail.com>
2024-12-18 18:13:45 -0800

    endpoints

--- endpoints/help.js
+++
@@ -1,7 +0,0 @@
-// Endpoint.
-const help = require("./createHelpEndpoint")("/help", "help.js");
-
-// Export.
-module.exports = Object.freeze(Object.defineProperty(help, "help", {
-  value: help
-}));
