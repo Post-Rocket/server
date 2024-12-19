8c1d85e983d653b3f3b05927361ad60824db66a2
William Brendel <brendel.william@gmail.com>
2024-12-18 17:18:11 -0800

    endpoints

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const expressListEndpoints = require('express-list-endpoints');
 const endpoints = require("./endpoints") || [];
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
