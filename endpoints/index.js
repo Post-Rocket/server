0e881dd85c50fb4280991bc56509645f2a8990a9
William Brendel <brendel.william@gmail.com>
2024-12-18 18:11:49 -0800

    endpoints

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,11 +1,10 @@
 const endpoints = module.exports = require("./getEndpoints")();
 
-let hasRoot = false;
+let hasRoot = false, hasHelp = false;
 for (let i = 0, l = endpoints.length; i !== l; ++i) {
-  if (endpoints[i].route === "/") {
-    hasRoot = true;
-    break;
-  }
+  endpoints[i].route === "/" && (hasRoot = true);
+  endpoints[i].route === "/help" && (hasHelp = true);
 }
 
 hasRoot || endpoints.unshift(require("./createHelpEndpoint")("/", "/"));
+hasHelp || endpoints.push(require("./createHelpEndpoint")("/help", "/help"));
