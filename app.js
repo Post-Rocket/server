2bb057ac9ae415f82a5d153d3968b1165a224958
William Brendel <brendel.william@gmail.com>
2024-12-26 14:19:12 -0800

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const endpoints = require("./endpoints") || [];
 const app = express();
-const PORT = 443;
+const PORT = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
